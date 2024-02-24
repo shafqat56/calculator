5a828946ab97d92e9ec84a05bd89fb29ea9c1366
shafqat56 <105715427+shafqat56@users.noreply.github.com>
2024-02-24 23:27:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let input = document.getElementById("input");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      input.value = eval(string);
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    }
+    else if (e.target.innerHTML == "DEL") {
+      string = string.slice(0, -1);
+      input.value = string;
+    }
+    else {
+        string += e.target.innerHTML;
+        input.value = string;
+        }
+  });
+});
